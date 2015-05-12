a7c8dafed65e8ceea2c28ae3344f56e6dad2deb7
efe.girek <efe.girek@gmail.com>
2015-05-12 15:35:53 +0300

    Butcele IOS scenario fixes.

--- features/butcele.feature
+++ features/butcele.feature
@@ -2,10 +2,7 @@
 @butcele
 Feature: Butcele test cases
 
-  Scenario: Successful Login
-    Given phone and pass info:
-        |  Phone    |   7997991111  |
-        |  Pass     |   456123      |
-
-    When nofitications allowed
-    Then should be able to login
+  Scenario: Login and create monthly budget expenses
+    Given phone and pin info "7997991111" "456123"
+    When new budget named "2014 May" created and the budget is "1100"
+    Then should be able to add an expense
--- features/steps/butcele_steps.py
+++ features/steps/butcele_steps.py
@@ -2,112 +2,88 @@ from behave import *
 from time import sleep
 from appium import webdriver
 
-"""    BDD         """
-"""    BUTCELE     """
-
-@given('we have behave installed')
-def step_impl(context):
-    pass
-
-@when('we implement a test')
-def step_impl(context):
-    assert True is not False
-
-@then('behave will test it for us!')
-def step_impl(context):
-    assert context.failed is False
-
-#context.driver  =webdriver.Remote()
+"""        BDD             """
+"""        BUTCELE         """
+"""        Scenario        """
+x_path = "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/"
 
 
-"""    Successful Login        """
-"""        Scenario            """
-log_inf = {'user': '', 'pass': ''}
-Phone   = "7997991111"
-Pass    = "456123"
-@given('phone and pass info')
-def pass_info(context):
-    pass
-   # for row in context.table:
-   #     print (row['Phone'])
 
-@when('nofitications allowed')
-def notification_pass(context):
+"""  LOGIN  """
+"""  In login section all the elements
+    assigned to a variable to send
+    click & send_key actions """
 
-    x_path = "//UIAApplication[1]/UIAWindow[1]/UIAScrollView[1]/UIAWebView[1]/"
+@given('phone and pin info "{phone}" "{pin}"')
+def login_section(context, phone, pin):
 
     Notify = context.driver.find_element_by_xpath('//UIAApplication[1]/UIAWindow[6]/UIAAlert[1]/UIACollectionView[1]/UIACollectionCell[2]')
     Notify.click()
     sleep(1)
 
-    """  LOGIN  """
+    """ enter phone number """
     t_phone = context.driver.find_element_by_xpath(x_path+'UIATextField[1]')
     t_phone.click()
     sleep(2)
-    t_phone.send_keys(Phone)
+    t_phone.send_keys(phone)
 
     t_button = context.driver.find_element_by_xpath(x_path+'UIAButton[1]')
     t_button.click()
     sleep(1)
 
+    """ enter pin number """
     t_pin = context.driver.find_element_by_xpath(x_path+'UIATextField[1]')
     t_pin.click()
-    t_pin.send_keys(Pass)
+    t_pin.send_keys(pin)
 
+    """" login  """
     pin_button = context.driver.find_element_by_xpath(x_path+'UIAButton[1]')
     pin_button.click()
-
-
-    """  NEW  BUDGET  """
-    tap_screen = context.driver.find_element_by_xpath(x_path+'UIAStaticText[5]')
-    tap_screen.click()
-
-    new_butce = context.driver.find_element_by_xpath(x_path+'UIAStaticText[3]')
-    new_butce.click()
-
-    b_name = context.driver.find_element_by_xpath(x_path+'UIATextField[1]')
-    b_name.click()
     sleep(1)
-    b_name.send_keys('New Budget')
 
-    b_button = context.driver.find_element_by_xpath(x_path+'UIAButton[2]')
-    b_button.click()
 
-    b_limit = context.driver.find_element_by_xpath(x_path+'UIATextField[1]')
-    b_limit.click()
+"""  NEW  BUDGET  """
+"""  In create budget section
+    actions directly send from
+    find element method """
+
+@when('new budget named "{b_name}" created and the budget is "{budget}"')
+def bugdet_section(context, b_name, budget):
+
+    context.driver.find_element_by_xpath(x_path+'UIAStaticText[5]').click()
+    context.driver.find_element_by_xpath(x_path+'UIAStaticText[3]').click()
+    context.driver.find_element_by_xpath(x_path+'UIATextField[1]').click()
+    context.driver.find_element_by_xpath(x_path+'UIATextField[1]').send_keys(b_name)
+    context.driver.find_element_by_xpath(x_path+'UIAButton[2]').click()
+    context.driver.find_element_by_xpath(x_path+'UIATextField[1]').click()
+    context.driver.find_element_by_xpath(x_path+'UIATextField[1]').send_keys(budget)
+    context.driver.find_element_by_xpath(x_path+'UIAButton[2]').click()
+    context.driver.find_element_by_xpath(x_path+'UIAButton[2]').click()
     sleep(1)
-    b_limit.send_keys('1100')
 
-    b_add = context.driver.find_element_by_xpath(x_path+'UIAButton[2]')
-    b_add.click()
-
-    b_kaydet = context.driver.find_element_by_xpath(x_path+'UIAButton[2]')
-    b_kaydet.click()
-    sleep(2)
-
-
-    """" NEW EXPENSE """
+    """ enters expense menu """
     context.driver.tap([(259, 503)], 1)
+    sleep(1)
 
-    e_limit = context.driver.find_element_by_xpath(x_path+'UIATextField[1]')
-    e_limit.click()
-    e_limit.send_keys('330')
 
-    new_ex = context.driver.find_element_by_xpath(x_path+'UIAButton[3]')
-    new_ex.click()
+"""" NEW EXPENSE """
+"""  In add budget expense section
+    actions send from
+    find element method.
+    also appium's driver.tap action
+    used for unvisible elements """
+@then('should be able to add an expense')
+def expense_section(context):
 
-    b_details = context.driver.find_element_by_xpath(x_path+'UIALink[1]/UIALink[1]/UIALink[2]/UIAStaticText[1]')
-    b_details.click()
+    context.driver.tap([(10, 103)], 1)
+    sleep(1)
+    context.driver.find_element_by_xpath(x_path+'UIATextField[1]').send_keys('330')
+    sleep(1)
+    context.driver.find_element_by_xpath(x_path+'UIAButton[3]').click()
+    sleep(1)
+    context.driver.find_element_by_xpath(x_path+'UIALink[1]/UIALink[1]/UIALink[2]/UIAStaticText[1]').click()
     sleep(10)
 
 
-
-
-@then('should be able to login')
-def login_to_butcele(context):
-    pass
-
-   # el = context.driver.find_element_by_xpath('//UIAMapView[1]')
-   # location = el.location
-   # context.driver.swipe(start_x=location['x'], start_y=location['y'], end_x=0.5, end_y=location['y'], duration=800)
-   # sleep(2)
+if __name__ == '__main__':
+    context.driver = webdriver.Remote()
